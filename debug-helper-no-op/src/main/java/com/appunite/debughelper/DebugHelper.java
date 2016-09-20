9b3a7dfced4c421eab380f0acab5911736d80a8c
Kuba Żerko <zerasop@gmail.com>
2016-09-20 15:32:52 +0200

    cleaned libraries

--- debug-helper-no-op/src/main/java/com/appunite/debughelper/DebugHelper.java
+++ debug-helper-no-op/src/main/java/com/appunite/debughelper/DebugHelper.java
@@ -2,13 +2,10 @@ package com.appunite.debughelper;
 
 import com.appunite.debughelper.interceptor.SampleInterceptor;
 
-import javax.annotation.Nonnull;
-
 import okhttp3.Interceptor;
 
 public class DebugHelper {
 
-    @Nonnull
     public static Interceptor getResponseInterceptor() {
         return new SampleInterceptor();
     }
