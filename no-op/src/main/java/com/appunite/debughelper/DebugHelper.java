bb74c436c52dc38adc89809813fbfe98164059f7
Kuba Żerko <zerasop@gmail.com>
2016-09-22 14:53:38 +0200

    changed module names, added empty install() metod to no-op

--- no-op/src/main/java/com/appunite/debughelper/DebugHelper.java
+++ no-op/src/main/java/com/appunite/debughelper/DebugHelper.java
@@ -4,10 +4,16 @@ import com.appunite.debughelper.interceptor.SampleInterceptor;
 
 import okhttp3.Interceptor;
 
+/**
+ * A no-op version of DebugHelper that can be used in release builds.
+ */
+
 public class DebugHelper {
 
     public static Interceptor getResponseInterceptor() {
         return new SampleInterceptor();
     }
 
+    public static void install(Context context) {
+    }
 }
